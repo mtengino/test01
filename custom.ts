5e04c50aa206bb6fb4b863961b12cc1ca8f8b9bd
mtengino <88091203+mtengino@users.noreply.github.com>
2022-07-25 16:20:46 +0300

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -2,7 +2,7 @@
  * Custom blocks
  */
 
-//% weight=1000 color=#CD5C5C icon="<font-awesome-icon icon="\uf121"
+//% weight=1000 color=#CD5C5C icon="<font-awesome-icon icon="fa-light fa-fish" />"
 namespace EnginoBit {
     /**
       * Turn on led for duration
