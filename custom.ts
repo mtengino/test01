1128342a31d6b10f9de18d79fa9d10cb49516e39
mtengino <88091203+mtengino@users.noreply.github.com>
2022-07-25 12:36:25 +0300

    Update main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -13,10 +13,17 @@ enum millis {
 //% weight=1000 color=#00AFF0 icon="\uf121"
 namespace EnginoBit {
     /**
-      * Set Duration Blinking
+      * Turn on led for duration
       * @param x millis
       */
     //% block="Blink with delay"
-    export function blink_led(x: millis): void {
+    export function led_on_for_duration(x: millis): void {
+    }
+
+    /**
+      * Turn off led
+      */
+    //% block="Led Off"
+    export function led_off(): void {
     }
 }
--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
 forever(function () {
-	
+    EnginoBit.blink_led()
 })
