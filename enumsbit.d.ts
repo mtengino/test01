da68042001a0409e7d9e23ebfbb3d81ce9f5ca5f
mtengino <88091203+mtengino@users.noreply.github.com>
2022-07-25 14:21:09 +0300

    Update pxt.json, main.ts, enumsbit.d.ts

---
+++ enumsbit.d.ts
@@ -0,0 +1,17 @@
+declare enum speed {
+    //% block="low"
+    Low,
+    //% block="medium"
+    Medium,
+    //% block="high"
+    High
+}
+
+declare enum pin {
+    //% block="Pin 2"
+    PIN_P2,
+    //% block="Pin 3"
+    PIN_P3,
+    //% block="Pin 4"
+    PIN_P4
+}
--- main.ts
+++ main.ts
@@ -1,3 +1,2 @@
 forever(function () {
-    EnginoBit.led_on_for_duration(pin.PIN_P2, 1)
 })
